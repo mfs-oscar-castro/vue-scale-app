f36f405d0086e4c5a606df9d54f8a3d3461ce43a
Oscar Castro <ocjuarez@tigo.com.gt>
2023-03-13 16:38:54 -0600

    Added vuex and & loader

--- vue3-project/src/store/index.ts
+++ vue3-project/src/store/index.ts
@@ -1,13 +1,68 @@
 import { createStore } from 'vuex'
+import { ItemInterface } from '@/models/items/Item.interface';
+import { ItemStateInterface } from '@/models/store/ItemState.interface';
+
+// Estado inicial
+const state: ItemStateInterface = {
+  loading: false,
+  items: []
+}
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
+  state: state,
   mutations: {
+    loadingItems(state: ItemStateInterface){
+      state.loading = true;
+      state.items = [];
+    },
+    loadedItems(state: ItemStateInterface, items: ItemInterface[]){
+      state.items = items;
+      state.loading = false;
+    },
+    selectItem(state: ItemStateInterface, params: {
+      id: number,
+      selected: boolean
+    }){
+      const { id, selected } = params;
+      const item = state.items.find(i => i.id === id);
+      if(item){
+        item.selected = selected;
+      }
+    }
   },
   actions: {
+    loadItems({ commit, state }){
+      commit('loadingItems');
+
+      // Datos falsos
+      const mockItems: ItemInterface[] = [
+        {
+          id: 1,
+          name: 'Item 1',
+          selected: false
+        },
+        {
+          id: 2,
+          name: 'Item 2',
+          selected: false
+        },
+        {
+          id: 3,
+          name: 'Item 3',
+          selected: false
+        }
+      ];
+
+      setTimeout(() => {
+        commit('loadedItems', mockItems)
+      }, 1000);
+    },
+    selectItem({ commit }, params: {
+      id: number,
+      selected: boolean
+    }){
+      commit('selectItem', params);
+    }
   },
   modules: {
   }
--- vue3-project/src/views/HomeView.vue
+++ vue3-project/src/views/HomeView.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="home">
-    <ItemListComponent :items="items"></ItemListComponent>
+    <ItemListComponent
+        :items="items"
+        :loading="loading"
+        @selectItem="onSelectItem" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from 'vue';
+import { defineComponent, computed, onMounted } from 'vue';
 import ItemListComponent from '@/components/item/ItemList.component.vue';
-import { ItemInterface } from '@/models/items/Item.interface'
+import { ItemInterface } from '@/models/items/Item.interface';
+import store from '@/store';
 
 export default defineComponent({
   name: 'Home',
@@ -15,22 +19,29 @@ export default defineComponent({
     ItemListComponent
   },
   setup(){
-    const items: ItemInterface[] = reactive([{
-      id: 1,
-      name: 'Item 1',
-      selected: false
-    }, {
-      id: 2,
-      name: 'Item 2',
-      selected: false
-    }, {
-      id: 3,
-      name: 'Item 3',
-      selected: false
-    }])
+    const items = computed(() => {
+      return store.state.items;
+    });
+
+    const loading = computed(() => {
+      return store.state.loading;
+    });
+
+    onMounted(() => {
+      store.dispatch('loadItems')
+    });
+
+    const onSelectItem = (item: ItemInterface) => {
+      store.dispatch('selectItem', {
+        id: item.id,
+        selected: !item.selected
+      });
+    }
 
     return {
-      items
+      items,
+      loading,
+      onSelectItem
     }
   }
 });
